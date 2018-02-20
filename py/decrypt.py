fc713e96102948053806fef9b002c14050dddceb
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2018-02-20 16:43:34 +1100

    Update decrypt.py

--- py/decrypt.py
+++ py/decrypt.py
@@ -1,7 +1,7 @@
 SUBSTITUTIONS = [['a','4'], ['e','3'], ['g', '6'], ['i','1'], ['o','0'], ['s', '5'], ['t','7'], ['A','4'], ['E','3'], ['G', '6'], ['I','1'], ['O','0'], ['S', '5'], ['T','7']]
 def decrypt(message, SUBSTITUTIONS):
   """ Use : decrypt("m355463", SUBSTITUTIONS)
-  => 'm355463'
+  => 'message'
   """
   for element in substitutions:
     old = element[1]
