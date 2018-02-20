72a594ee9c92d9cac25ff2e551b2c5608fee76b4
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2018-02-20 16:43:01 +1100

    decrypt.py leet

---
+++ py/decrypt.py
@@ -0,0 +1,10 @@
+SUBSTITUTIONS = [['a','4'], ['e','3'], ['g', '6'], ['i','1'], ['o','0'], ['s', '5'], ['t','7'], ['A','4'], ['E','3'], ['G', '6'], ['I','1'], ['O','0'], ['S', '5'], ['T','7']]
+def decrypt(message, SUBSTITUTIONS):
+  """ Use : decrypt("m355463", SUBSTITUTIONS)
+  => 'm355463'
+  """
+  for element in substitutions:
+    old = element[1]
+    new = element[0]
+    message = message.replace(old, new)
+  return message
